7ec78c384257196f549eef9abc0f17bb3676c449
Lucky <sashankaswain6@gmail.com>
2025-07-11 11:50:59 +0530

    Added Notfound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import './App.css'
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Auth } from "@/pages/Auth/Auth";
-import { SignupCard } from '@/components/organisms/Auth/SignupCard';
-import { SigninCard } from '@/components/organisms/Auth/SigninCard';
+import { SignupCard } from "@/components/organisms/Auth/SignupCard";
+import { SigninCard } from "@/components/organisms/Auth/SigninCard";
+import { NotFound } from "@/pages/NotFound/NotFound";
 
 function App() {
   return (
-   <Routes>
-    
-    <Route path="/auth/signup" element={<Auth><SignupCard /></Auth>} />
-    <Route path="/auth/signin" element={<Auth><SigninCard /></Auth>} />
+    <Routes>
+      <Route
+        path="/auth/signup"
+        element={
+          <Auth>
+            <SignupCard />
+          </Auth>
+        }
+      />
+      <Route
+        path="/auth/signin"
+        element={
+          <Auth>
+            <SigninCard />
+          </Auth>
+        }
+      />
 
-   </Routes>
-  )
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
