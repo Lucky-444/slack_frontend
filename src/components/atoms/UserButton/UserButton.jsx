a7888fc431f10c81efee2cacd11d1cc0b3b1923c
Lucky <sashankaswain6@gmail.com>
2025-07-12 15:46:23 +0530

    bug fixed

--- src/components/atoms/UserButton/UserButton.jsx
+++ src/components/atoms/UserButton/UserButton.jsx
@@ -7,13 +7,19 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { useAuth } from "@/hooks/context/useAuth";
 import { useToast } from "@/hooks/use-toast";
-import { LogOutIcon, SettingsIcon } from "lucide-react";
+import { LogOutIcon, SettingsIcon, PencilIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import { useCreateWorkspaceModal } from "../../../hooks/context/useCreateWorkspaceModal";
 
 export const UserButton = () => {
   const navigate = useNavigate();
   const { auth, logout } = useAuth();
   const { toast } = useToast();
+  const { setOpenCreateWorkspaceModal } = useCreateWorkspaceModal();
+
+  function openWorkspaceCreateModal() {
+    setOpenCreateWorkspaceModal(true);
+  }
 
   async function handleLogout() {
     await logout();
@@ -22,7 +28,7 @@ export const UserButton = () => {
       status: "success",
       type: "success",
     });
-    navigate('/auth/signin');
+    navigate("/auth/signin");
   }
 
   return (
@@ -36,6 +42,10 @@ export const UserButton = () => {
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
+        <DropdownMenuItem onClick={openWorkspaceCreateModal}>
+          <PencilIcon className="size-4 mr-2 h-10" />
+          Create Workspace
+        </DropdownMenuItem>
         <DropdownMenuItem>
           <SettingsIcon className="size-4 mr-2 h-10" />
           Settings
