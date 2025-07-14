557ccd1eb6633d05f4c836fd19d9311248a40010
Lucky <sashankaswain6@gmail.com>
2025-07-14 17:41:05 +0530

    Added channel threads

--- src/components/organisms/Workspace/WorkspacePanel.jsx
+++ src/components/organisms/Workspace/WorkspacePanel.jsx
@@ -1,37 +1,40 @@
 import { useParams } from "react-router-dom";
 import { useGetWorkspaceById } from "../../../hooks/apis/workspaces/useGetWorkspaceById";
-import { AlertTriangleIcon, Loader } from "lucide-react";
+import { AlertTriangleIcon, Loader, MessageSquareTextIcon } from "lucide-react";
 import { WorkspacePanelHeader } from "../../molecules/workspace/WorkspacePanelHeader";
+import { SideBarItem } from "../../atoms/SideBarItem/SideBarItem";
 
 export const WorkspacePanel = () => {
   const { workspaceId } = useParams();
-  const { workspace , isFetching , isSuccess} = useGetWorkspaceById(workspaceId);
-    if(isFetching) {
+  const { workspace, isFetching, isSuccess } = useGetWorkspaceById(workspaceId);
+  if (isFetching) {
+    return (
+      <div className="flex flex-col gap-y-2 h-full items-center justify-center text-white">
+        <Loader className="animate-spin size-6 text-white" />
+      </div>
+    );
+  }
 
-        return (
-            <div
-                className='flex flex-col gap-y-2 h-full items-center justify-center text-white'
-            >
-                <Loader className='animate-spin size-6 text-white' />
-            </div>
-        );
-    }
-
-     if(!isSuccess) {
-        return (
-            <div
-                className='flex flex-col gap-y-2 h-full items-center justify-center text-white'
-            >
-                <AlertTriangleIcon className='size-6 text-white' />
-                Something went wrong
-            </div>
-        );
-    }
-
+  if (!isSuccess) {
+    return (
+      <div className="flex flex-col gap-y-2 h-full items-center justify-center text-white">
+        <AlertTriangleIcon className="size-6 text-white" />
+        Something went wrong
+      </div>
+    );
+  }
 
   return (
-  <div className="flex flex-col h-full bg-slack-medium">
-        <WorkspacePanelHeader  workspace={workspace}/>
-  </div>
-  )
+    <div className="flex flex-col h-full bg-slack-medium">
+      <WorkspacePanelHeader workspace={workspace} />
+      <div className="flex flex-col px-2 mt-3">
+        <SideBarItem
+          label="Threads"
+          icon={MessageSquareTextIcon}
+          id="threads"
+          variant="active"
+        />
+      </div>
+    </div>
+  );
 };
